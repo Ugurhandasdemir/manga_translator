3f337dad9b93b714ae919edb332f33c5b00ea695
Uğurhan Daşdemir <ugurhandasdemir@gmail.com>
2026-08-11 21:13:51 +0300

    c++ read image

--- c++/build/CMakeFiles/manga.dir/src/main.cpp.o.d
+++ c++/build/CMakeFiles/manga.dir/src/main.cpp.o.d
@@ -360,4 +360,7 @@ CMakeFiles/manga.dir/src/main.cpp.o: \
  /usr/include/opencv4/opencv2/stitching/detail/camera.hpp \
  /usr/include/opencv4/opencv2/video.hpp \
  /usr/include/opencv4/opencv2/video/tracking.hpp \
- /usr/include/opencv4/opencv2/video/background_segm.hpp
+ /usr/include/opencv4/opencv2/video/background_segm.hpp \
+ /usr/include/c++/13/filesystem /usr/include/c++/13/bits/fs_fwd.h \
+ /usr/include/c++/13/bits/fs_path.h /usr/include/c++/13/codecvt \
+ /usr/include/c++/13/bits/fs_dir.h /usr/include/c++/13/bits/fs_ops.h
--- c++/src/main.cpp
+++ c++/src/main.cpp
@@ -1,19 +1,36 @@
-#include<opencv2/opencv.hpp>
-#include<iostream>
+#include <opencv2/opencv.hpp>
+#include <iostream>
+#include <filesystem>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <fstream>
 
 using namespace std;
-using namespace cv;
+namespace fs = std::filesystem;
+
 
 int main(int argc, char** argv){
 
-    Mat img = imread("/mnt/windows/linux/Python/manga/test/1.jpg",1);
+    string img_folder = "/mnt/windows/linux/Python/manga/test";
+    vector<string> images;
 
-    if(img.empty()){
-        cout<<"Could not read image "<<endl;
-        return 1;
+    for (const auto& kayit : fs::directory_iterator(img_folder)){
+        
+        if(!kayit.is_regular_file()){
+                     continue;
+        }
+
+        string uzanti = kayit.path().extension().string();
+
+        if(uzanti == ".jpg" || uzanti == ".jpge" || uzanti == ".png"){
+            images.push_back(kayit.path().string());
+        }
     }
 
-    imshow("Display window", img);
-    waitKey(0);
-    destroyAllWindows();
-}
+        for (int i = 0; i < images.size(); i++){
+         cout << "[" << i + 1 << "/" << images.size() << "] "
+             << images[i] << endl;
+        }
+    }
+
